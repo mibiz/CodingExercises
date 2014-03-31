429d4dfdf0130e5969ac92a73ec5e489bbd4a832
Mel Ibizugbe <broxdoc@gmail.com>
2014-03-31 13:42:33 -0400

    Style tweaks

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -11,21 +11,20 @@ int main(int argc, char** argv) {
     int mul_three = 3;
     int mul_five = 5;
     
-        for (i = 1; i < 101; i++) {
-			if ((i != mul_three) && (i != mul_five)) {
-				cout << i << endl;
-			}
-			else {
-				if(i == mul_three) {
-					cout << "Fizz";
-					mul_three+= 3;
-				}
-				if(i == mul_five) {
-					cout << "Buzz";
-					mul_five+= 5;
-				}
-				cout << endl;
-			}
+    for (i = 1; i < 101; i++) {
+        if ((i != mul_three) && (i != mul_five)) {
+            cout << i << endl;
         }
-
+        else {
+            if(i == mul_three) {
+                cout << "Fizz";
+                mul_three+= 3;
+            }
+            if(i == mul_five) {
+                cout << "Buzz";
+                mul_five+= 5;
+            }
+            cout << endl;
+        }
+    }
 }
