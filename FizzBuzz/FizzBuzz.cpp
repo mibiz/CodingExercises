a90c192b072cf727c058a6aeac8c436b581c18ad
Mel Ibizugbe <broxdoc@gmail.com>
2014-04-02 22:16:09 -0400

    Added unit testing

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <sstream>
+#include <cstring>
+#include "cppunit/TestFixture.h"
+#include "cppunit/TestCaller.h"
+#include "cppunit/TextTestRunner.h"
+#include "cppunit/TextOutputter.h"
 using namespace std;
 
 /**
@@ -6,7 +12,7 @@ using namespace std;
  * of three it prints "Fizz" instead of the number, and for the multiples of five it prints "Buzz". 
  * For numbers which are multiples of both three and five it prints "FizzBuzz".
  */
-int main(int argc, char** argv) {
+void FizzBuzz() {
     int i;
     int mul_three = 3;
     int mul_five = 5;
@@ -27,4 +33,56 @@ int main(int argc, char** argv) {
             cout << endl;
         }
     }
+}
+
+/** Test the FizzBuzz function by redirecting its stdout to a
+ *  private string buffer. We will then test the contents of this
+ *  buffer if it has the correct output.
+ */
+class TestFizzBuzz : public CppUnit::TestFixture {
+private:
+    stringstream oss;
+    streambuf* tmp_cout_buf;
+    
+public:
+    TestFizzBuzz() {}
+    
+    void setUp() {
+        //redirect cout to oss
+        tmp_cout_buf = cout.rdbuf();
+        cout.rdbuf(oss.rdbuf());
+    }
+    
+    void testFizzBuzz() {
+        FizzBuzz();
+        int t;
+        string s;
+        //read the first line into integer
+        oss >> t;
+        CPPUNIT_ASSERT( t == 1 );
+        //read the second line into integer
+        oss >> t;
+        CPPUNIT_ASSERT (t == 2);
+        //the third line should say "Fizz"
+        oss >> s;
+        CPPUNIT_ASSERT( s == "Fizz" );
+        oss >> t;
+        CPPUNIT_ASSERT( t == 4 );
+        oss >> s;
+        //fifth line should say "Buzz"
+        CPPUNIT_ASSERT( s == "Buzz" );
+    }
+    
+    void tearDown() {
+        //un-redirect cout
+        cout.rdbuf(tmp_cout_buf);
+    }
+};
+
+int main (int argc, char** argv) {
+    CppUnit::Test *test = new CppUnit::TestCaller<TestFizzBuzz>( "testFizzBuzz", &TestFizzBuzz::testFizzBuzz );
+    CppUnit::TextTestRunner runner;
+    runner.addTest(test);
+    runner.run();
+    return 0;
 }
